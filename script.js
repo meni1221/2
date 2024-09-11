3e97f2d6af2bb31c6967d4928eddd4a16fbe562b
mennnndy <mennnndy@levi.com>
2024-09-11 15:06:25 +0300

    js

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
+//Task 1
+
 const filterAndSortEvenNumbers = (x) => x.filter((x) => x % 2 === 0).sort();
 console.log(filterAndSortEvenNumbers([3, 51, 3, 27, 22, 10, 32]));
 
+//Task 2
 const removeDuplicates = (x) => [...new Set(x)];
 
 console.log(removeDuplicates([2, 2, 3, 4, 4, 55, 55]));
+
+//Task 3
+
